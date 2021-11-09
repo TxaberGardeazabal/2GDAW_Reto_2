bbf1f1997955a542d30c1526a0b90002a93750da
TxaberGardeazabal <73885809+TxaberGardeazabal@users.noreply.github.com>
2021-11-09 12:58:53 +0100

    testeado, resuelto bugs, cambiado la estructura de la base

--- reto2.sql
+++ reto2.sql
@@ -4,22 +4,26 @@ CREATE TABLE categorias (
   clase VARCHAR(20) PRIMARY KEY
 );
 
+CREATE TABLE usuarios (
+  id INT(4) AUTO_INCREMENT PRIMARY KEY,
+  nomUsuario VARCHAR(20) UNIQUE,
+  contrasena VARCHAR(20)
+);
+
 CREATE TABLE comerciantes (
   id INT(4) PRIMARY KEY,
-  nomUsuario VARCHAR(20) UNIQUE,
-  contrasena VARCHAR(20),
   nomEmpresa VARCHAR(50),
   telefono VARCHAR(9),
   codPostal INT(5),
   email VARCHAR(35),
+  CONSTRAINT COME_US_FK FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
 CREATE TABLE compradores (
-  id INT(4) PRIMARY KEY
-  nomUsuario VARCHAR(20) UNIQUE,
-  contrasena VARCHAR(20),
+  id INT(4) PRIMARY KEY,
   nombre VARCHAR(20),
-  email VARCHAR(30)
+  email VARCHAR(30),
+  CONSTRAINT COMp_US_FK FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
 CREATE TABLE anuncios (
@@ -32,14 +36,15 @@ CREATE TABLE anuncios (
   visitas INT,
   categoria varchar(20),
   comerciante INT,
-  CONSTRAINTS ANUN_CAT_FK FOREIGN KEY (categoria) REFERENCES categorias(clase),
-  CONSTRAINTS ANUN_COM_FK FOREIGN KEY (comerciante) REFERENCES comerciantes(id)
+  CONSTRAINT ANUN_CAT_FK FOREIGN KEY (categoria) REFERENCES categorias(clase),
+  CONSTRAINT ANUN_COM_FK FOREIGN KEY (comerciante) REFERENCES comerciantes(id) ON DELETE CASCADE
 );
 
 CREATE TABLE destacados (
   idCli INT(4),
   idAnun INT(4),
-  CONSTRAINTS DES_PK PRIMARY KEY (idCli,idAnun),
-  CONSTRAINTS DES_COMP_FK FOREIGN KEY (idCli) REFERENCES compradores(id) ON DELETE CASCADE,
-  CONSTRAINTS DES_ANUN_FK FOREIGN KEY (idAnun) REFERENCES anuncios(id) ON DELETE CASCADE,
+  CONSTRAINT DES_PK PRIMARY KEY (idCli,idAnun),
+  CONSTRAINT DES_COMP_FK FOREIGN KEY (idCli) REFERENCES compradores(id) ON DELETE CASCADE,
+  CONSTRAINT DES_ANUN_FK FOREIGN KEY (idAnun) REFERENCES anuncios(id) ON DELETE CASCADE
 );
+
