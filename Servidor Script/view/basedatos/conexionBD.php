fefe3f1d4e1e8963221c7e912dc8a521fc7e982f
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2021-11-22 08:49:42 +0100

    Login y Anuncios

--- Servidor Script/view/basedatos/conexionBD.php
+++ Servidor Script/view/basedatos/conexionBD.php
@@ -1,8 +1,8 @@
 <?php
 $host="127.0.0.1";
 $dbname="web_anuncios";
-$user="2gdaw";
-$pass="12345Abcde";
+$user="root";
+$pass="";
 
 $baseDatos = conectar($host,$dbname,$user,$pass);
 
@@ -32,19 +32,12 @@ function generarDatos($baseDatos,$arrayCat,$i){
 function generarDatos2($baseDatos,$dato){
     $arrayAnun = selectCompleja($baseDatos,"anuncios","nombre","categoria",$dato);
     for($j= 0; $j<count($arrayAnun); $j++){
-        
+        $posicion = fswitch($j);
     
     echo"<a href=\"anuncios.php?anun=$arrayAnun[$j] \"class=\"anuncio\">$arrayAnun[$j]</a>";
     }
 }
-function generarDatos3($baseDatos,$dato){
-    $arrayAnun = selectCompleja($baseDatos,"anuncios","nombre","categoria",$dato);
-    for($j= 0; $j<count($arrayAnun); $j++){
-        $posicion = fswitch($j);
-    
-    echo"<a href=\"anuncios.php?anun=$arrayAnun[$j] \"class=\"anuncio $posicion\">$arrayAnun[$j]</a>";
-    }
-}
+
 
 function generarBotonesDatos($baseDatos,$arraySupCat,$i){
     $arrayCat = selectCompleja($baseDatos,"categorias","clase","superclase",$arraySupCat[$i]);
@@ -57,16 +50,6 @@ function generarBotonesDatos($baseDatos,$arraySupCat,$i){
 function select($baseDatos, $tabla,$columna){
     $categorias = array();
     $statement = $baseDatos->prepare("SELECT $columna FROM $tabla");
-    $statement->execute();
-    
-    while($row = $statement->fetch()) {
-        array_push($categorias, $row["$columna"]);
-    }
-    return($categorias);
-}
-function selectSencilla($baseDatos,$tabla,$columna,$aComparar ,$dato){
-    $categorias = array();
-    $statement = $baseDatos->prepare("SELECT $columna FROM $tabla WHERE $aComparar = \"$dato\"");
     $statement->execute();
     
     while($row = $statement->fetch()) {
@@ -156,36 +139,4 @@ function fswitch($i){
             break;    
     }
     return $posicion;
-}
-
-////////////// prubeba de imagen
-function cosoImagen() {
-    $archivo = $_FILES['imagen']['name'];
-
-    if (isset($archivo) && $archivo != "") {
-        $tipo = $_FILES['imagen']['type'];
-        $tamano = $_FILES['imagen']['size'];
-        $temp = $_FILES['imagen']['tmp_name'];
-
-        echo $archivo;
-        // validar
-
-        if(move_uploaded_file($temp, "imagenes/".$archivo)) {
-            echo "fue bien?";
-            chmod('imagenes/'.$archivo, 0777);
-            echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
-            echo '<p><img src="imagenes/'.$archivo.'"></p>';
-        }
-        else {
-            echo "problemas al subir imagen";
-        }
-    }
-    else {
-        echo "a";
-    }
-}
-
-if (isset($_POST["imagen"])) {
-    echo "a";
-    cosoImagen();
 }
---
+++ Servidor Script/view/basedatos/uploadImagen.php
@@ -0,0 +1,27 @@
+<?php
+$target_dir = "../imagenes/";
+$target_file = $target_dir . basename($_FILES["imagen"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$mensaje="";
+
+if (file_exists($target_file)) {
+  $mensaje =  "La imagen ya existe. ";
+  $uploadOk = 0;
+}
+
+$uploadOk == 0 ? $mensaje = $mensaje . "Lo sentimos, la imagen no ha sido subida." : $mensaje = subirFichero($target_file,$mensaje);
+
+function subirFichero($target_file,$mensaje){
+    if(move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)){
+        $mensaje = "El fichero '". htmlspecialchars( basename( $_FILES["imagen"]["name"])). "' ha sido subido."; 
+    }else{
+        $mensaje = "Lo sentimos, ha habido un error al subir el archivo";
+    }
+    return $mensaje;
+}
+
+echo $mensaje;
+?>
+
+
