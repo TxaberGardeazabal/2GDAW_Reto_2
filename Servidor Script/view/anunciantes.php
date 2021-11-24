871e934439eee71af16cef8392ec8b2af4db25c5
txaber <txaber.musico@gmail.com>
2021-11-24 10:38:41 +0100

    anunciantes validado

--- Servidor Script/view/anunciantes.php
+++ Servidor Script/view/anunciantes.php
@@ -57,9 +57,9 @@
                     }
                     $html = '<div class="container_anuncio"><section class="div_anuncio">'.
                     '<h3>'.$anun["nombre"].'</h3>'.
-                    '<div class="imagen"><img src="url('.$anun["imagen"].')" alt="imagen"></div>'.
+                    '<div class="imagen"><img src="'.$anun["imagen"].'" alt="imagen"></div>'.
                     '<div class="datos">'.
-                    '<ul><li>Precio: '.$anun["precio"].'&euro;</li><li>REF: '.$refs["nomEmpresa"].'</li><li>Localizacion: '.$anun["localizacion"].'</li><li>Descripcion: '.$anun["descripcion"].'</li></ul>'.
+                    '<ul><li>Precio: '.$anun["precio"].'&euro;</li><li>REF: '.$refs["telefono"].'</li><li>Localizacion: '.$anun["localizacion"].'</li><li>Descripcion: '.$anun["descripcion"].'</li></ul>'.
                     '</div></section><div class="div_boton"><button onclick="verAnuncio(\''.$anun["id"].'\')">Ver en la web</button><button onclick="borrarAnuncio(\''.$anun["id"].'\')">Eliminar</button></div></div>';
                     echo $html;
                 }
@@ -90,15 +90,15 @@
         </div>
         <div id="div_formulario">
             <h2>Nuevo anuncio</h2>
-            <form method="POST" action="basedatos/conexionBD.php" enctype="multipart/form-data">
+            <form method="POST" action="basedatos/conexionBD.php" enctype="multipart/form-data" id="formulario">
                 <label for="tit">Titulo:</label>
-                <input type="text" name="nombre" id="tit" maxlength="20">
+                <input type="text" name="nombre" id="tit" maxlength="30">
                 <label for="ref">Ref:</label>
-                <input type="text" name="ref" id="ref" maxlength="20" disabled>
+                <input type="text" name="ref" id="ref" value="<?php echo $refs["telefono"]?>" disabled>
                 <label for="emp">Empresa:</label>
-                <input type="text" name="empresa" id="emp" maxlength="20" disabled>
+                <input type="text" name="empresa" id="emp" value="<?php echo $refs["nomEmpresa"]?>" disabled>
                 <label for="loc">Localizacion:</label>
-                <input type="text" name="loc" id="loc" maxlength="20">
+                <input type="text" name="loc" id="loc" maxlength="30">
                 <label for="pre">Precio:</label>
                 <input type="text" name="precio" id="pre" placeholder="&euro;" maxlength="9">
                 <label for="img">Imagen:</label>
@@ -146,19 +146,24 @@
             const imagen = $("#img").val();
             const extensionImagen=  imagen.split('.')[1];
             const urlImagen = imagen.substr(imagen.lastIndexOf('\\') + 1).split('.')[0] + "." + extensionImagen;
-            if(!titulo || !localizacion || !precio || !categoria || !descripcion){
-                Swal.fire({
-                    icon: 'error',
-                    text: 'Rellena todos los campos',
-                }); 
-            }else{
+
+            try {
+                if(!titulo || !localizacion || !precio || !categoria || !descripcion){
+                    throw new Error("rellena todos los campos");
+                }
+
+                let regex = new RegExp("^[0-9]{1,7}(\.[0-9]{2})?$");
+                if(!regex.test(precio)) {
+                    throw new Error("precio no correcto");
+                }
+
                 $.ajax({    
                     type: "POST",
                     dataType: "text",
                     data: formData,
                     url: "./basedatos/uploadImagen.php",             
                     success: function (respuesta){
-                        if(respuesta=="La imagen ya existe. Lo sentimos, la imagen no ha sido subida." || respuesta=="Lo sentimos, ha habido un error al subir el archivo"){
+                        if(respuesta=="La imagen ya existe. Lo sentimos, la imagen no ha sido subida." || respuesta=="Lo sentimos, ha habido un error al subir el archivo" || respuesta=="solo se admiten imagenes en formato .png o .jpg"){
                             Swal.fire({
                                 icon: 'error',
                                 title:"Error",
@@ -194,6 +199,13 @@
                     contentType: false,
                     processData: false  
                 });
+                
+            }
+            catch (err) {
+                Swal.fire({
+                        icon: 'error',
+                        text: err,
+                    }); 
             }
         });
     </script>
--- Servidor Script/view/basedatos/uploadImagen.php
+++ Servidor Script/view/basedatos/uploadImagen.php
@@ -1,27 +1,36 @@
 <?php
-$target_dir = "../imagenes/";
-$target_file = $target_dir . basename($_FILES["imagen"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$mensaje="";
+/*$tipo = $_FILES['imagen']['type'];
+$tamano = $_FILES['imagen']['size'];
+//Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+if (!(strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png"))) {
+    return "solo se admiten imagenes en formato .png o .jpg";
+}else {*/
 
-if (file_exists($target_file)) {
-  $mensaje =  "La imagen ya existe. ";
-  $uploadOk = 0;
-}
+    $target_dir = "imagenes/subidas/";
 
-$uploadOk == 0 ? $mensaje = $mensaje . "Lo sentimos, la imagen no ha sido subida." : $mensaje = subirFichero($target_file,$mensaje);
+    $target_file = $target_dir . basename($_FILES["imagen"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $mensaje="";
 
-function subirFichero($target_file,$mensaje){
-    if(move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)){
-        $mensaje = "El fichero '". htmlspecialchars( basename( $_FILES["imagen"]["name"])). "' ha sido subido."; 
-    }else{
-        $mensaje = "Lo sentimos, ha habido un error al subir el archivo";
+    if (file_exists($target_file)) {
+    $mensaje =  "La imagen ya existe. ";
+    $uploadOk = 0;
     }
-    return $mensaje;
-}
 
-echo $mensaje;
+    $uploadOk == 0 ? $mensaje = $mensaje . "Lo sentimos, la imagen no ha sido subida." : $mensaje = subirFichero($target_file,$mensaje);
+
+    function subirFichero($target_file,$mensaje){
+        if(move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)){
+            $mensaje = "El fichero '". htmlspecialchars( basename( $_FILES["imagen"]["name"])). "' ha sido subido."; 
+        }else{
+            $mensaje = "Lo sentimos, ha habido un error al subir el archivo";
+        }
+        return $mensaje;
+    }
+
+    echo $mensaje;
+//}
 ?>
 
 
